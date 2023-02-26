416719e0a6a74a3ca44fdfd6044fe412b5b8cce7
hadisptr <anandahadisaputra@gmail.com>
2023-02-26 23:42:46 +0700

    f

---
+++ client/PartsClient/PartsClient/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PartsClient.Platforms.MacCatalyst;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
